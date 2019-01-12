b2adadb0078a0bd38112fff93424ea9e9080e296
Julien Lamy <lamy@unistra.fr>
2019-01-12 20:55:29 +0100

    Fix TimeInterval unit tests

--- tests/cpp/TimeInterval.cpp
+++ tests/cpp/TimeInterval.cpp
@@ -8,7 +8,8 @@ BOOST_AUTO_TEST_CASE(ScalarScalarConstructor)
 {
     sycomore::TimeInterval const interval(1., 2.);
     BOOST_TEST(interval.duration == 1.);
-    BOOST_TEST(interval.gradient_moment == sycomore::Array<sycomore::Real>({2,2,2}));
+    BOOST_TEST(
+        interval.gradient_moment == sycomore::Array<sycomore::Real>({2,2,2}));
 }
 
 BOOST_AUTO_TEST_CASE(UnitScalarConstructor)
@@ -16,14 +17,16 @@ BOOST_AUTO_TEST_CASE(UnitScalarConstructor)
     using namespace sycomore::units;
     sycomore::TimeInterval const interval(1._ms, 2./dm);
     BOOST_TEST(interval.duration == 1.e-3);
-    BOOST_TEST(interval.gradient_moment == sycomore::Array<sycomore::Real>({2,2,2}));
+    BOOST_TEST(
+        interval.gradient_moment == sycomore::Array<sycomore::Real>({20,20,20}));
 }
 
 BOOST_AUTO_TEST_CASE(ScalarVectorConstructor)
 {
     sycomore::TimeInterval const interval(1., {2,3,4});
     BOOST_TEST(interval.duration == 1.);
-    BOOST_TEST(interval.gradient_moment == sycomore::Array<sycomore::Real>({2,3,4}));
+    BOOST_TEST(
+        interval.gradient_moment == sycomore::Array<sycomore::Real>({2,3,4}));
 }
 
 BOOST_AUTO_TEST_CASE(UnitVectorConstructor)
@@ -31,5 +34,6 @@ BOOST_AUTO_TEST_CASE(UnitVectorConstructor)
     using namespace sycomore::units;
     sycomore::TimeInterval const interval(1._ms, {2/dm,4/m,8/dam});
     BOOST_TEST(interval.duration == 1.e-3);
-    BOOST_TEST(interval.gradient_moment == sycomore::Array<sycomore::Real>({2,3,4}));
+    BOOST_TEST(
+        interval.gradient_moment == sycomore::Array<sycomore::Real>({20,4,0.8}));
 }
