d9d6857ad8c5b28af2c86ddd86ebd695d0029e32
Julien Lamy <lamy@unistra.fr>
2019-02-25 16:25:25 +0100

    Make Quantity hashable in Python

--- wrappers/python/Quantity.cpp
+++ wrappers/python/Quantity.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <sstream>
 
 #include <pybind11/pybind11.h>
@@ -5,6 +6,42 @@
 
 #include "sycomore/Dimensions.h"
 #include "sycomore/Quantity.h"
+
+#include <iostream>
+
+namespace std
+{
+
+template<>
+struct hash<sycomore::Quantity>
+{
+    // Not a good hash function, but easy to test
+    size_t operator()(sycomore::Quantity const & q)
+    {
+        auto const k1 = std::hash<double>{}(q.magnitude);
+        // FIXME: k2 is in Z not in N
+        auto k2 =
+            (q.dimensions.length << 1)
+            + (q.dimensions.mass << 2)
+            + (q.dimensions.time << 3)
+            + (q.dimensions.electric_current << 4)
+            + (q.dimensions.thermodynamic_temperature << 5)
+            + (q.dimensions.amount_of_substance << 6)
+            + (q.dimensions.luminous_intensity << 7);
+        if(k2 < 0)
+        {
+            k2 = -2*k2+1;
+        }
+        else
+        {
+            k2 = 2*k2;
+        }
+        // Cantor's pairing function
+        return std::hash<double>{}(0.5*(k1+k2)*(k1+k2+1) + k2);
+    }
+};
+
+}
 
 void wrap_Quantity(pybind11::module & m)
 {
@@ -46,5 +83,6 @@ void wrap_Quantity(pybind11::module & m)
                 std::ostringstream s;
                 s << d;
                 return s.str();
-            });
+            })
+        .def(hash(self));
 }
