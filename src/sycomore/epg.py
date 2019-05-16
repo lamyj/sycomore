4287579b90bfbf454ca9291f76d53562489e1b67
Julien Lamy <lamy@unistra.fr>
2019-05-16 12:01:52 -0400

    Fix EPG gradients and relaxation

--- src/sycomore/epg.py
+++ src/sycomore/epg.py
@@ -1,5 +1,5 @@
 import numpy
-from numpy import cos, exp, sin
+from numpy import cos, exp, pi, sin
 from sycomore.units import *
 
 class State(object):
@@ -14,28 +14,30 @@ class State(object):
         return self._magnetization[:,:self._size]
     
     def apply_pulse(self, angle, phase=0*deg):
-        alpha = angle.magnitude
-        phi = phase.magnitude
+        a = angle.convert_to(rad)
+        p = phase.convert_to(rad)
     
         M = numpy.asarray([
-            [cos(alpha/2)**2, exp(2j*phi)*sin(alpha/2)**2, -1j*exp(1j*phi)*sin(alpha)],
-            [exp(-2j*phi)*sin(alpha/2)**2, cos(alpha/2)**2, 1j*exp(-1j*phi)*sin(alpha)],
-            [-1j/2*exp(-1j*phi)*sin(alpha), 1j/2*exp(1j*phi)*sin(alpha), cos(alpha)]
+            [cos(a/2)**2, exp(2j*p)*sin(a/2)**2, -1j*exp(1j*p)*sin(a)],
+            [exp(-2j*p)*sin(a/2)**2, cos(a/2)**2, 1j*exp(-1j*p)*sin(a)],
+            [-1j/2*exp(-1j*p)*sin(a), 1j/2*exp(1j*p)*sin(a), cos(a)]
         ])
         
         self._magnetization[:,:self._size] = M @ self.magnetization
     
     def apply_gradient(self):
         # TODO: resize factor
-        self._magnetization = numpy.concatenate(
-            (self._magnetization, numpy.zeros((3, 10), dtype=numpy.complex)), 
-            axis=1)
+        if self._size >= self._magnetization.shape[1]:
+            self._magnetization = numpy.concatenate(
+                (self._magnetization, numpy.zeros((3, 10), dtype=numpy.complex)), 
+                axis=1)
         
-        refocalizing = self._magnetization[1, 1]
-        
+        # Shift positive F̃ states right
         self._magnetization[0, 1:1+self._size] = self._magnetization[0, 0:self._size]
+        # Shift negative F̃^* states left
         self._magnetization[1, 0:self._size] = self._magnetization[1, 1:1+self._size]
-        self._magnetization[:2, 0] = refocalizing
+        # Update F̃_{+0} using F̃^*_{-0}
+        self._magnetization[0, 0] = numpy.conj(self._magnetization[1, 0])
         
         self._size += 1
     
@@ -44,4 +46,4 @@ class State(object):
         E_2 = exp((-duration/species.T2).magnitude)
         E = numpy.diag([E_2, E_2, E_1])
         self._magnetization[:,:self._size] = E @ self.magnetization
-        self._magnetization[2,:self._size] += 1-E_1 # WARNING: assumes M0=1
+        self._magnetization[2,0] += 1-E_1 # WARNING: assumes M0=1
