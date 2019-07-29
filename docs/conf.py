3dd3600c1ec9d15e8ee02c680df029e8722b8006
Julien Lamy <lamy@unistra.fr>
2019-07-29 23:13:49 +0200

    Bump version: 0.5.0 → 0.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, Julien Lamy'
 author = 'Julien Lamy'
 
 # The short X.Y version
-version = '0.5.0'
+version = '0.6.0'
 # The full version, including alpha/beta/rc tags
 release = version
 
