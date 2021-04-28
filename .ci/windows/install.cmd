87305f81cc7e18c86a71b54e86a49763262fc75d
Julien Lamy <lamy@unistra.fr>
2021-04-28 17:10:08 +0200

    CI: fix numpy install on Windows

--- .ci/windows/install.cmd
+++ .ci/windows/install.cmd
@@ -1,12 +1,3 @@
 REM Assume a working conda environment
-conda install --yes -c conda-forge cmake ninja pybind11 xsimd
+conda install --yes -c conda-forge cmake ninja numpy pybind11 xsimd
 if %errorlevel% neq 0 exit /b %errorlevel%
-
-%PYTHON% -m pip install --upgrade pip
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-REM Assume Python later than 3.5
-%PYTHON% -m pip install -U numpy
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-cd %WORKSPACE%
