8fcd86ee676112f7ab6727fb930febda5284b646
Julien Lamy <lamy@unistra.fr>
2021-04-28 17:07:26 +0200

    CI: fix unattended install on Windows

--- .ci/windows/install.cmd
+++ .ci/windows/install.cmd
@@ -1,5 +1,5 @@
 REM Assume a working conda environment
-conda install -c conda-forge cmake ninja pybind11 xsimd
+conda install --yes -c conda-forge cmake ninja pybind11 xsimd
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 %PYTHON% -m pip install --upgrade pip
