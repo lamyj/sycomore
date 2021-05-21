d9ef040d14b23116a43a3cc53e428e9d57786c2d
Julien Lamy <lamy@unistra.fr>
2021-05-21 20:14:30 +0200

    Windows CI on Github

--- .ci/windows/build.cmd
+++ .ci/windows/build.cmd
@@ -7,10 +7,8 @@ mkdir %INSTALL_DIR%
 cd %BUILD_DIR%
 
 cmake ^
-  -G Ninja ^
   -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
   -DCMAKE_BUILD_TYPE=Release ^
-  -DBOOST_ROOT="C:/Libraries/boost_1_67_0" ^
   %CMAKE_OPTIONS% ^
   ..
 if %errorlevel% neq 0 exit /b %errorlevel%
