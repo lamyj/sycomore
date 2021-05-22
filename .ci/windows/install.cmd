39799205b093dd6da1ac96c6e8525d75138ca637
Julien Lamy <lamy@unistra.fr>
2021-05-22 09:38:06 +0200

    Add missing dependency

--- .ci/windows/install.cmd
+++ .ci/windows/install.cmd
@@ -1,3 +1,3 @@
 REM Assume a working conda environment
-conda install --yes -c conda-forge cmake ninja numpy pybind11 xsimd
+conda install --yes -c conda-forge boost cmake ninja numpy pybind11 xsimd
 if %errorlevel% neq 0 exit /b %errorlevel%
