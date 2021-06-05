62f3fbd9671c67cd0f71ff008120b32c89b89d30
Julien Lamy <lamy@unistra.fr>
2021-06-05 07:54:59 +0200

    Fix typo [no ci]

--- .ci/wheels/wheel.py
+++ .ci/wheels/wheel.py
@@ -6,5 +6,5 @@ import setup_python
 
 version = sys.argv[1]
 interpreter = setup_python.setup_python(version)
-process = subprocess.check_call([
-    interpreter, "install.py", cwd=os.path.dirname(__file__)])
+process = subprocess.check_call(
+    [interpreter, "install.py"], cwd=os.path.dirname(__file__))
