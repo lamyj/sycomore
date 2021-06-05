9cff9ab11163d75f58e38db1a70263a7ec693d93
Julien Lamy <lamy@unistra.fr>
2021-06-05 11:36:03 +0200

    Fix wheels on manylinux1 [no ci]

--- .ci/wheels/wheel.py
+++ .ci/wheels/wheel.py
@@ -4,6 +4,14 @@ import sys
 
 import setup_python
 
+if sys.version_info < (2, 6):
+    def check_call(*args, **kwargs):
+        retcode = subprocess.call(*args, **kwargs)
+        if retcode:
+            raise Exception()
+        return 0
+    subprocess.check_call = check_call
+
 version = sys.argv[1]
 interpreter = setup_python.setup_python(version)
 process = subprocess.check_call([interpreter, ".ci/wheels/install.py"])
