2db0a03c2c98f91ffbe3fe1cd2149614fa5af4ab
Julien Lamy <lamy@unistra.fr>
2021-06-07 10:15:20 +0200

    Fix PATH [no ci]

--- .ci/build/post_build.py
+++ .ci/build/post_build.py
@@ -8,6 +8,7 @@ workspace = os.environ["WORKSPACE"]
 build_dir = os.environ.get("BUILD_DIR", os.path.join(workspace, "build"))
 install_dir = os.environ.get("BUILD_DIR", os.path.join(workspace, "install"))
 
+bin_dir = os.path.join(install_dir, "bin")
 lib_dir = os.path.join(install_dir, "lib")
 python_lib_dir = os.path.join(
     install_dir, distutils.sysconfig.get_python_lib(True, prefix=""))
@@ -19,6 +20,8 @@ print(lib_dir, python_lib_dir, python_tests_dir)
 for name in ["DYLD_LIBRARY_PATH", "LD_LIBRARY_PATH"]:
     os.environ[name] = os.pathsep.join([
         *os.environ.get(name, "").split(os.pathsep), lib_dir])
+os.environ["PATH"] = os.pathsep.join([
+    *os.environ.get("PATH", "").split(os.pathsep), bin_dir])
 os.environ["PYTHONPATH"] = os.pathsep.join([
     *os.environ.get("PYTHONPATH", "").split(os.pathsep), python_lib_dir])
 os.environ["SYCOMORE_TEST_DATA"] = os.path.join(workspace, "tests", "data")
