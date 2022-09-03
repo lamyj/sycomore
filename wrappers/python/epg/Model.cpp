f80f592df4d848b66241198604dc1a509634092a
Julien Lamy <lamy@unistra.fr>
2022-09-03 12:53:09 +0200

    Wrap epg::Model::Kind

---
+++ wrappers/python/epg/Model.cpp
@@ -0,0 +1,21 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+
+#include "sycomore/epg/Model.h"
+
+void wrap_epg_Model(pybind11::module & m)
+{
+    using namespace pybind11;
+    using namespace sycomore;
+    using namespace sycomore::epg;
+    
+    auto model = class_<Model>(
+        m, "Model",
+        "Model a single- or two-pool system expressed in EPG formalism");
+    enum_<Model::Kind>(model, "Kind")
+        .value("SinglePool", Model::SinglePool)
+        .value("Exchange", Model::Exchange)
+        .value("MagnetizationTransfer", Model::MagnetizationTransfer)
+        .export_values();
+}
--- wrappers/python/epg/epg.cpp
+++ wrappers/python/epg/epg.cpp
@@ -3,12 +3,14 @@
 void wrap_epg_Base(pybind11::module &);
 void wrap_epg_Discrete(pybind11::module &);
 void wrap_epg_Discrete3D(pybind11::module &);
+void wrap_epg_Model(pybind11::module &);
 void wrap_epg_Regular(pybind11::module &);
 
 void wrap_epg(pybind11::module & m)
 {
     auto epg = m.def_submodule("epg");
     
+    wrap_epg_Model(epg);
     wrap_epg_Base(epg);
     wrap_epg_Discrete(epg);
     wrap_epg_Discrete3D(epg);
