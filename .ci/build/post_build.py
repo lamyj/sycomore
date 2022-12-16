f577d3e10dd1541c9a3eb935de009822b6955636
Julien Lamy <lamy@unistra.fr>
2022-12-16 12:40:32 +0100

    Prefer local install in tests

--- .ci/build/post_build.py
+++ .ci/build/post_build.py
@@ -14,16 +14,14 @@ python_lib_dir = os.path.join(
     install_dir, distutils.sysconfig.get_python_lib(True, prefix=""))
 python_tests_dir = os.path.join(workspace, "tests", "python")
 
-print(lib_dir, python_lib_dir, python_tests_dir)
-
 # Set-up environment: C++ library, Python module and test data location.
 for name in ["DYLD_LIBRARY_PATH", "LD_LIBRARY_PATH"]:
     os.environ[name] = os.pathsep.join([
-        *os.environ.get(name, "").split(os.pathsep), lib_dir])
+        lib_dir, *os.environ.get(name, "").split(os.pathsep)])
 os.environ["PATH"] = os.pathsep.join([
-    *os.environ.get("PATH", "").split(os.pathsep), bin_dir])
+    bin_dir, *os.environ.get("PATH", "").split(os.pathsep)])
 os.environ["PYTHONPATH"] = os.pathsep.join([
-    *os.environ.get("PYTHONPATH", "").split(os.pathsep), python_lib_dir])
+    python_lib_dir, *os.environ.get("PYTHONPATH", "").split(os.pathsep)])
 os.environ["SYCOMORE_TEST_DATA"] = os.path.join(workspace, "tests", "data")
 
 # Run C++ and Python tests even if the former fails, return non-zero if any
