774d1915c371fd87857750fc4004df7dea4cf74c
Julien Lamy <lamy@unistra.fr>
2022-12-16 09:48:14 +0100

    Update conda dependencies

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -3,8 +3,8 @@ import sys
 
 # NOTE: restrict xsimd version on macOS due to
 # https://github.com/xtensor-stack/xsimd/issues/810
-xsimd_version = "<9.0.0,>9.0.1" if sys.platform == "darwin" else ""
+xsimd_version = "<9.0.0|>9.0.1" if sys.platform == "darwin" else ""
 subprocess.check_call([
     "conda", "install", "--yes", "-c", "conda-forge",
     "boost", "cmake", "ninja", "numpy", "pybind11", "scipy",
-    f"xsimd{xsimd_version}"])
+    f"xsimd{xsimd_version}", "xtensor", "xtensor-python"])
