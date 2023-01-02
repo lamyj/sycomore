9908ed8e3e275ccd2ff715382a6d214fb56baad3
Julien Lamy <lamy@unistra.fr>
2023-01-02 10:07:50 +0100

    Require xsimd>9.0.1

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -1,10 +1,7 @@
 import subprocess
 import sys
 
-# NOTE: restrict xsimd version on macOS due to
-# https://github.com/xtensor-stack/xsimd/issues/810
-xsimd_version = "<9.0.0|>9.0.1" if sys.platform == "darwin" else ""
 subprocess.check_call([
     "conda", "install", "--yes", "-c", "conda-forge",
-    "boost", "cmake", "ninja", "numpy", "pybind11", "scipy",
-    f"xsimd{xsimd_version}", "xtensor", "xtensor-python"])
+    "boost", "cmake", "ninja", "numpy", "pybind11", "scipy", "xsimd>9.0.1",
+    "xtensor", "xtensor-python"])
