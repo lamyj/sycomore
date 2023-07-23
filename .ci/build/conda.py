4dc1544d4a61614046cd2357c5a1a0e904dae6ef
Julien Lamy <lamy@unistra.fr>
2023-07-23 09:08:06 +0200

    Re-add xsimd version constraint

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -3,5 +3,5 @@ import sys
 
 subprocess.check_call([
     "conda", "install", "--yes", "-c", "conda-forge",
-    "boost", "cmake", "ninja", "numpy", "pybind11", "scipy", "xsimd",
+    "boost", "cmake", "ninja", "numpy", "pybind11", "scipy", "xsimd>9.0.1",
     "xtensor", "xtensor-python"])
