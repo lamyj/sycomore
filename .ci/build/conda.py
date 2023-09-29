5c9fcb5ca0412b7f960ad672d63408f682c7553d
Julien Lamy <lamy@unistra.fr>
2023-09-29 10:43:18 +0200

    Add missing dependencies

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -5,5 +5,5 @@ conda = sys.argv[1] if len(sys.argv) >= 2 else "conda"
 
 subprocess.check_call([
     conda, "install", "--yes", "-c", "conda-forge",
-    "boost", "cmake", "ninja", "numpy", "pybind11", "scipy", "xsimd",
-    "xtensor", "xtensor-python"])
+    "boost", "boost-cpp", "cmake", "ninja", "numpy", "pybind11", "scipy",
+    "xsimd", "xtensor", "xtensor-python"])
