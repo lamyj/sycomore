5c367882d22b989d7b8dc8ca27383e4ee1fc573d
Julien Lamy <lamy@unistra.fr>
2023-09-29 10:13:56 +0200

    Switch to micromamba

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -1,7 +1,9 @@
 import subprocess
 import sys
 
+conda = sys.argv[1] if len(sys.argv) >= 2 else "conda"
+
 subprocess.check_call([
-    "conda", "install", "--yes", "-c", "conda-forge",
+    conda, "install", "--yes", "-c", "conda-forge",
     "boost", "cmake", "ninja", "numpy", "pybind11", "scipy", "xsimd",
     "xtensor", "xtensor-python"])
