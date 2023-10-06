dbf4cc0f0ea42222e656875e225cf3f7a3b12aad
Julien Lamy <lamy@unistra.fr>
2023-10-06 17:34:07 +0200

    Fix variable name

--- .ci/readthedocs/build.py
+++ .ci/readthedocs/build.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 subprocess.check_call([
-    "cmake", "-S", ".", f"-DPYTHON_EXECUTABLE={sys.executable}",
+    "cmake", "-S", ".", f"-DPython_EXECUTABLE={sys.executable}",
     "-DCMAKE_BUILD_TYPE=Release", f"-DCMAKE_INSTALL_PREFIX={sys.exec_prefix}",
     "-DBUILD_TESTING=OFF", "-B", "build"])
 subprocess.check_call([
